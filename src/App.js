04c4e485a01d045c5da93e1b6b178b72272ce2eb
Sokafootball <laurent.capello@gmail.com>
2019-01-24 14:45:26 +0100

    added fetch to the api

--- src/App.js
+++ src/App.js
@@ -4,6 +4,23 @@ import Title from './components/Title'
 import Game from './components/Game'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      countries: []
+    }
+  }
+
+  componentDidMount(){
+    const countriesAPI = 'https://restcountries.eu/rest/v2/all'
+    fetch(countriesAPI)
+      .then(data => data.json())
+      .then(result => {
+        console.log(result[0])
+        this.setState({countries: result})
+       })
+  }
+
   render() {
     return (
       <div className="App">
