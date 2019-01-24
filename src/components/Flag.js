d52964c00cc8dd8ccb04b1443e20d2109b0d55b0
Sokafootball <laurent.capello@gmail.com>
2019-01-24 12:13:09 +0100

    added component boilerplates and css files

---
+++ src/components/Flag.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Flag.css'
+
+const Flag = (props) => {
+  return(
+    <div>
+
+    </div>
+  )
+}
+
+export default Flag
---
+++ src/components/Guesses.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Guesses.css'
+
+const Guesses = (props) => {
+  return(
+    <div>
+
+    </div>
+  )
+}
+
+export default Guesses
